a07d18a6ed848e9ca371cfe3e4654402508d207b
97proh <guilherme1706@hotmail.com>
2023-02-08 18:47:24 -0300

    operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String meuNome = "Guilherme Alves";
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+
+        int numero = 5;
+
+        numero = 10;
+        System.out.println(numero);
+
+        /*Valor com final não pode se alterado */
+        final double VALOR_DE_PI = 3.14; 
+
+       
     }
 }
